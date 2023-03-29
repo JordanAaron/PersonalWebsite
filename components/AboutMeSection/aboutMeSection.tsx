ff3a9be7b43c511c2c6310c8f6c128425f471dce
Jordan Quartey <jordev14@gmail.com>
2023-03-29 21:20:07 +0100

    refactor: move elements around to place title and description in same div for better responsive work

--- components/AboutMeSection/aboutMeSection.tsx
+++ components/AboutMeSection/aboutMeSection.tsx
@@ -29,12 +29,14 @@ type Props = {
 export const AboutMeSection = ({ title, profile, contactIcons, card }: Props) => {
   return (
     <section className='pt-10'>
-      <p className={`text-2xl font-semibold ${styles.title}`}>{title}</p>
-      <div className={`grid grid-cols-4 gap-x-1 my-4`}>
-        <div className={`col-span-3 ${styles.profileContainer}`}>
-          <p>{profile}</p>
+      <div className={`grid grid-cols-4 gap-x-1`}>
+        <div className='col-span-3'>
+          <p className={`${styles.title} text-2xl font-semibold `}>{title}</p>
+          <div className={`${styles.profileContainer} my-4`}>
+            <p>{profile}</p>
+          </div>
         </div>
-        <div className={`col-span-1 justify-self-end`}>
+        <div className={`col-span-1 justify-self-end my-auto`}>
           {contactIcons.map((icon: Icon) => {
             const  { entryTitle, iconImage: { description, url }} = icon
             return (
