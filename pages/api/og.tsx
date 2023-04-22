f3b9f1dea98554ca76345f54908032f36bcca4de
Jordan Quartey <jordev14@gmail.com>
2023-04-22 22:17:31 +0100

    refactor: og image

--- pages/api/og.tsx
+++ pages/api/og.tsx
@@ -12,24 +12,21 @@ export default function (): ImageResponse {
   try {
     return new ImageResponse(
       (
-        <div
-          style={{
-            fontSize: 128,
-            background: 'white',
-            width: '100%',
-            height: '100%',
-            display: 'flex',
-            textAlign: 'center',
-            alignItems: 'center',
-            justifyContent: 'center'
-          }}>
-          Hello world!
-        </div>
-      ),
-      {
-        width: 1200,
-        height: 600
-      }
+        // <div
+        //   style={{
+        //     fontSize: 128,
+        //     background: 'white',
+        //     width: '100%',
+        //     height: '100%',
+        //     display: 'flex',
+        //     textAlign: 'center',
+        //     alignItems: 'center',
+        //     justifyContent: 'center'
+        //   }}>
+        //   Hello world!
+        // </div>
+        <div>Hello world!</div>
+      )
     )
   } catch (e) {
     console.log(`${(e as Error).message}`)
