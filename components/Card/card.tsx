6e137c8405da615a7974ac5cdb5a492d291ffb73
Jordan Quartey <jordev14@gmail.com>
2023-08-04 23:54:51 +0100

    feat: center text on card with image

--- components/Card/card.tsx
+++ components/Card/card.tsx
@@ -69,7 +69,7 @@ export const Card = ({ card }: Props): JSX.Element => {
         className={`
         ${styles.descriptionContainer}  
         ${cardConfig[`${cardColor}` as keyof CardConfig].color}
-        ${cardImage ? '' : 'h-full p-4'} 
+        ${cardImage ? 'text-center' : 'h-full p-4'} 
       `}>
         <CardContent cardContentEntries={cardContentEntries} />
       </div>
